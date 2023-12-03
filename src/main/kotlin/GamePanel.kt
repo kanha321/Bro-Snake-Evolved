ff1ea0de25a287b421b8c7ff1c23ecd2e139ac4d
Kanha <shubh.sg321@gmail.com>
2023-12-03 16:23:47 +0530

    added png for apples

--- src/main/kotlin/GamePanel.kt
+++ src/main/kotlin/GamePanel.kt
@@ -1,15 +1,15 @@
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Font
-import java.awt.Graphics
+import java.awt.*
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 import java.util.*
+import javax.imageio.ImageIO
 import javax.swing.JPanel
 import javax.swing.Timer
 import kotlin.system.exitProcess
+
+
 
 
 class GamePanel : JPanel(), ActionListener {
@@ -23,7 +23,7 @@ class GamePanel : JPanel(), ActionListener {
     val y = IntArray(gameUnits)
     val bigAppleSize = 3
     val bigAppleUnitSize = bigAppleSize * unitSize
-    val bigAppleFrequency = 0
+    val bigAppleFrequency = 5
     val bigAppleScore = 5
     val bigAppleBodyParts = 1
     val bigAppleTimer = 5000
@@ -32,6 +32,8 @@ class GamePanel : JPanel(), ActionListener {
     var speedCheck = 0
     var appleX = 0
     var appleY = 0
+    var collisionX = -1
+    var collisionY = -1
     var bigAppleX = 0
     var bigAppleY = 0
     var scoreCount = 0
@@ -43,14 +45,46 @@ class GamePanel : JPanel(), ActionListener {
     var stopMovement = false
     var timer: Timer? = null
     var random: Random? = null
+    private val appleImg: Image = ImageIO.read(GamePanel::class.java.getResource("/apple2.png"))
+    private val bigAppleImg: Image = ImageIO.read(GamePanel::class.java.getResource("/bigApple.png"))
+    private val snakeHeadImg: Image = ImageIO.read(GamePanel::class.java.getResource("/snakeHead.jpg"))
 
     init {
         random = Random()
         val keyAd = MyKeyAdapter()
         this.preferredSize = Dimension(screenWidth, screenHeight)
-        setBackground(Color.BLACK)
+        setBackground(Color(0))
         setFocusable(true)
         addKeyListener(keyAd)
+        startGame()
+    }
+
+
+
+    fun startGame() {
+        newBigApple()
+        newApple()
+//        y[0] = (screenHeight / unitSize) / 2 * unitSize
+        running = true
+        timer = Timer(delay, this)
+        timer!!.start()
+    }
+
+    fun restartGame() {
+        bodyParts = initialBodyParts
+        scoreCount = 0
+        appleAfterBigApple = 0
+        stopMovement = false
+        collisionX = -1
+        collisionY = -1
+        x[0] = 0
+        y[0] = 0
+        for (i in 1..<bodyParts) {
+            x[i] = -1
+            y[i] = -1
+        }
+        direction = 'r'
+        gameOver = false
         startGame()
     }
 
@@ -64,18 +98,27 @@ class GamePanel : JPanel(), ActionListener {
             if (showBigApple()) {
                 bigAppleTimer(g)
             }
-            g.color = Color.red
-            g.fillOval(appleX, appleY, unitSize, unitSize)
+//            g.color = Color.red
+//            g.fillOval(appleX, appleY, unitSize, unitSize)
+            g.drawImage(appleImg, appleX, appleY, unitSize, unitSize, null)
             if (showBigApple()) {
-                g.color = Color.yellow
-                g.fillOval(bigAppleX, bigAppleY, bigAppleUnitSize, bigAppleUnitSize)
+//                g.color = Color.yellow
+//                g.fillOval(bigAppleX, bigAppleY, bigAppleUnitSize, bigAppleUnitSize)
+                g.drawImage(bigAppleImg, bigAppleX, bigAppleY, bigAppleUnitSize, bigAppleUnitSize, null)
                 bigAppleTimer(g)
             }
             for (i in 0..<bodyParts) {
                 if (i == 0) {
-                    g.color = Color.green
+                    if (stopMovement && x[i] == collisionX && y[i] == collisionY)
+                        g.color = Color.red
+                    else
+                        g.color = Color.green
+//                        g.drawImage(snakeHeadImg, x[i], y[i], unitSize, unitSize, null)
                 } else {
-                    g.color = Color(45, 180, 0)
+                    if (stopMovement && x[i] == collisionX && y[i] == collisionY)
+                        g.color = Color.red
+                    else
+                        g.color = Color(45, 180, 0)
                 }
                 g.fillRect(x[i], y[i], unitSize, unitSize)
             }
@@ -84,88 +127,17 @@ class GamePanel : JPanel(), ActionListener {
         displayScore(g)
     }
 
-//    fun bigAppleTimer(g: Graphics) {
-//        g.color = Color(210, 144, 0, 180)
-//        g.font = Font("Monotype Corsiva", Font.PLAIN, 40)
-//        g.drawString(bigAppleTimeLeft.toString() + "", screenWidth - 100, screenHeight - 30)
-//    }
-
-
-    fun bigAppleTimer(g: Graphics) {
-        var i = 0
-        while (i < screenWidth) {
-            g.color = Color(210, 177, 0, 180)
-            g.fillRect(i, screenHeight - unitSize, unitSize, unitSize)
-            i += unitSize
-        }
-    }
-
-
-    fun displayScore(g: Graphics) {
-        g.color = Color(176, 176, 176, 180)
-        g.font = Font("Monotype Corsiva", Font.PLAIN, 40)
-        val metrics = getFontMetrics(g.font)
-        g.drawString(
-            "score: $scoreCount",
-            (screenWidth - metrics.stringWidth("score: $scoreCount")) / 2,
-            screenHeight - 30
-        )
-    }
-
     fun newApple() {
         appleX = random!!.nextInt(screenWidth / unitSize) * unitSize
         appleY = random!!.nextInt(screenHeight / unitSize) * unitSize
-    }
-
-    fun gameOver(g: Graphics) {
-        gameOver = true
-        g.color = Color.red
-        g.font = Font("Ink Free", Font.BOLD, 69)
-        val metrics = getFontMetrics(g.font)
-        g.drawString("Game Over!!", (screenWidth - metrics.stringWidth("Game Over")) / 2, screenHeight / 2)
-        restartPrompt(g)
-    }
-
-    fun restartPrompt(g: Graphics) {
-        g.color = Color.green
-        g.font = Font("Times New Roman", Font.PLAIN, 30)
-        val metrics = getFontMetrics(g.font)
-        g.drawString(
-            "Press SPACE key to play again",
-            (screenWidth - metrics.stringWidth("Press SPACE key to play again")) / 2,
-            screenHeight - 100
-        )
-    }
-
-    fun startGame() {
-        newBigApple()
-        newApple()
-        running = true
-        timer = Timer(delay, this)
-        timer!!.start()
-    }
-
-    fun restartGame() {
-        bodyParts = initialBodyParts
-        scoreCount = 0
-        appleAfterBigApple = 0
-        stopMovement = false
-        x[0] = 0
-        y[0] = 0
-        for (i in 1..<bodyParts) {
-            x[i] = -1
-            y[i] = -1
-        }
-        direction = 'r'
-        gameOver = false
-        startGame()
+//        appleY = screenHeight-unitSize
     }
 
     fun checkApple() {
         if (x[0] == appleX && y[0] == appleY) {
             newApple()
             bodyParts++
-            scoreCount++
+            scoreCount+=speed
             if (!showBigApple()) appleAfterBigApple++
         }
         for (i in bodyParts downTo 1) {
@@ -193,9 +165,9 @@ class GamePanel : JPanel(), ActionListener {
             var j = bigAppleY
             while (j < bigAppleY + bigAppleSize * unitSize) {
                 if (x[0] == i && y[0] == j) {
+                    scoreCount += bigAppleTimeLeft/100 * speed
                     newBigApple()
                     bodyParts += bigAppleBodyParts
-                    scoreCount += bigAppleScore
                     appleAfterBigApple = 0
                 }
                 j += unitSize
@@ -219,6 +191,8 @@ class GamePanel : JPanel(), ActionListener {
         }
         for (i in bodyParts downTo 1) {
             if (x[0] == x[i] && y[0] == y[i]) {
+                collisionX = x[i]
+                collisionY = y[i]
                 stopMovement = true
             }
             if (!running) timer!!.stop()
@@ -252,20 +226,71 @@ class GamePanel : JPanel(), ActionListener {
             if (speedCheck == 22 - speed * 3 || speedCheck == 0) {
                 if (stopMovement) die()
                 else move()
-                speedCheck = 0;
+                speedCheck = 0
             }
-            speedCheck++;
-            checkApple();
+            speedCheck++
+            checkApple()
             if (showBigApple()) {
-                checkBigApple();
-                bigAppleTimeLeft -= delay;
-                if (bigAppleTimeLeft <= 0) {
-                    newBigApple();
-                }
+                checkBigApple()
+                if (!stopMovement)
+                    bigAppleTimeLeft -= delay
+                if (bigAppleTimeLeft <= 0)
+                    newBigApple()
             }
-            checkCollision();
+            checkCollision()
         }
         repaint()
+    }
+
+    fun bigAppleTimer(g: Graphics){
+        bigAppleTimer1(g)
+        bigAppleTimer2(g)
+    }
+    fun bigAppleTimer1(g: Graphics) {
+        var i = 0
+        while (i < bigAppleTimeLeft * screenWidth / bigAppleTimer) {
+//            31D500FF
+            g.color = Color(255, 255, 0)
+            g.fillRect(i - unitSize, screenHeight - 3, unitSize, unitSize)
+            i++
+        }
+    }
+
+    fun bigAppleTimer2(g: Graphics) {
+        g.color = Color(210, 144, 0, 180)
+        g.font = Font("Monotype Corsiva", Font.PLAIN, 40)
+        g.drawString(bigAppleTimeLeft.toString() + "", screenWidth - 100, screenHeight - 30)
+    }
+
+    fun displayScore(g: Graphics) {
+        g.color = Color(200, 200, 200, 180)
+        g.font = Font("Monotype Corsiva", Font.PLAIN, 40)
+        val metrics = getFontMetrics(g.font)
+        g.drawString(
+            "score: $scoreCount",
+            (screenWidth - metrics.stringWidth("score: $scoreCount")) / 2,
+            screenHeight - 30
+        )
+    }
+
+    fun gameOver(g: Graphics) {
+        gameOver = true
+        g.color = Color.red
+        g.font = Font("Ink Free", Font.BOLD, 69)
+        val metrics = getFontMetrics(g.font)
+        g.drawString("Game Over!!", (screenWidth - metrics.stringWidth("Game Over")) / 2, screenHeight / 2)
+        restartPrompt(g)
+    }
+
+    fun restartPrompt(g: Graphics) {
+        g.color = Color.green
+        g.font = Font("Times New Roman", Font.PLAIN, 30)
+        val metrics = getFontMetrics(g.font)
+        g.drawString(
+            "Press SPACE key to play again",
+            (screenWidth - metrics.stringWidth("Press SPACE key to play again")) / 2,
+            screenHeight - 100
+        )
     }
 
 
@@ -276,6 +301,11 @@ class GamePanel : JPanel(), ActionListener {
                 KeyEvent.VK_S -> if (direction != 'u' && !stopMovement) direction = 'd'
                 KeyEvent.VK_A -> if (direction != 'r' && !stopMovement) direction = 'l'
                 KeyEvent.VK_D -> if (direction != 'l' && !stopMovement) direction = 'r'
+
+                KeyEvent.VK_UP -> if (direction != 'd' && !stopMovement) direction = 'u'
+                KeyEvent.VK_DOWN -> if (direction != 'u' && !stopMovement) direction = 'd'
+                KeyEvent.VK_LEFT -> if (direction != 'r' && !stopMovement) direction = 'l'
+                KeyEvent.VK_RIGHT -> if (direction != 'l' && !stopMovement) direction = 'r'
 
                 KeyEvent.VK_SPACE -> {
                     if (!running) {
@@ -306,21 +336,21 @@ class GamePanel : JPanel(), ActionListener {
                     restartGame()
                 }
 
-                KeyEvent.VK_F9 -> {
-                    if (running) {
-                        x[0] = appleX
-                        y[0] = appleY
-                        for (i in 1..<bodyParts) {
-                            when (direction) {
-                                'u' -> y[i] = appleY + unitSize
-                                'd' -> y[i] = appleY - unitSize
-                                'l' -> x[i] = appleX + unitSize
-                                'r' -> x[i] = appleX - unitSize
-                            }
-                        }
-                        checkApple()
-                    }
-                }
+//                KeyEvent.VK_F9 -> {
+//                    if (running) {
+//                        x[0] = appleX
+//                        y[0] = appleY
+//                        for (i in 1..<bodyParts) {
+//                            when (direction) {
+//                                'u' -> y[i] = appleY + unitSize
+//                                'd' -> y[i] = appleY - unitSize
+//                                'l' -> x[i] = appleX + unitSize
+//                                'r' -> x[i] = appleX - unitSize
+//                            }
+//                        }
+//                        checkApple()
+//                    }
+//                }
             }
         }
     }
